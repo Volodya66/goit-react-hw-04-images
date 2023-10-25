9aec86f473c9ab1f23e18c257c485d113eff0923
Volodya66 <130245015+Volodya66@users.noreply.github.com>
2023-10-25 14:12:10 +0300

    Fixed bug

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -6,7 +6,7 @@ export default function ImageGallery({ srs,alt,bigPhoto,getBigPhoto }) {
  
   return (
   <li key={bigPhoto} id={bigPhoto} onClick={getBigPhoto} className="ImageGalleryItem">
-   <img className="ImageGalleryItem-imag" src={srs} alt={alt} />
+   <img className="ImageGalleryItem-image" src={srs} alt={alt} />
   </li>
   );
 }
